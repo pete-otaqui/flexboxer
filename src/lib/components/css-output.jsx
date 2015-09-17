7b364f1dc5a3992e54ed33e57c5a3069137a2660
Pete Otaqui <pete@otaqui.com>
2015-09-17 17:43:04 +0100

    refactored CssRule into it's own class

--- src/lib/components/css-output.jsx
+++ src/lib/components/css-output.jsx
@@ -1,49 +1,15 @@
 import React from 'react';
 
-class CssRule extends React.Component {
-  render() {
-    let propNodes = [];
-    let prop, val;
-    for ( prop in this.props.properties ) {
-      val = this.props.properties[prop];
-      propNodes.push(
-        <div className="css-property" key={`prop-${prop}`}>
-          <div className="css-property-property">
-            {prop}:&nbsp;
-          </div>
-          <div className="css-property-value">
-            {val};
-          </div>
-        </div>
-      )
-    }
-    return <div className="css-rule">
-      <div className="css-selector">
-        {this.props.selector} {'{'}
-      </div>
-      <div className="css-properties">
-        {propNodes}
-      </div>
-      {'}'}
-    </div>;
-  }
-}
+import CssRule from './css-rule.jsx!';
 
 class CssOutput extends React.Component {
   render() {
-    // let ruleNodes = [];
-    // let key, rule;
-    // for (key in this.props.rules ) {
-    //   rule = this.props.rules[key];
-    //   ruleNodes.push(<CssRule {...rule} key={`rule-${key}`} />);
-    // };
-    console.log(this.props);
-    return (<div>
+    return <div>
       <h2>Css Output</h2>
       <div className="css-rules">{this.props.rules.map(function(rule) {
           return <CssRule {...rule} key={`rule-${rule.id}`} />;
         })}</div>
-    </div>);
+    </div>;
   }
 }
 
