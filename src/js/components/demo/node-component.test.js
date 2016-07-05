2172894bd719cd32260f52bd1a693fce64193c35
Pete Otaqui <pete.otaqui@gft.com>
2016-07-05 06:18:33 +0100

    Tidied up Node tests

--- src/js/components/demo/node-component.test.js
+++ src/js/components/demo/node-component.test.js
@@ -6,38 +6,24 @@ import Node from './node-component';
 
 tape('Adds a style attribute', (assert) => {
   assert.plan(1);
-  const node = {
-    style: { display: 'block' }
-  };
-  const wrapper = shallow(
-    <Node node={node} />
-  );
+  const node = { style: { display: 'block' } };
+  const wrapper = shallow( <Node node={node} /> );
   const html = wrapper.html();
   assert.ok(html.match(/style/).length, 'Adds a style attribute');
 });
 
 tape('Adds text content', (assert) => {
   assert.plan(1);
-  const node = {
-    textContent: 'Foo bar'
-  };
-  const wrapper = shallow(
-    <Node node={node} />
-  );
+  const node = { textContent: 'Foo bar' };
+  const wrapper = shallow( <Node node={node} /> );
   const html = wrapper.html();
   assert.ok(html.match(/Foo bar/), 'Adds text content');
 });
 
 tape('Adds child nodes', (assert) => {
   assert.plan(1);
-  const node = {
-    children: [
-      {}, {}, {}
-    ]
-  };
-  const wrapper = shallow(
-    <Node node={node} />
-  );
+  const node = { children: [ {}, {}, {} ] };
+  const wrapper = shallow( <Node node={node} /> );
   const childNodes = wrapper.find(Node);
   assert.equals(childNodes.length, 3, 'Adds child nodes');
 });
