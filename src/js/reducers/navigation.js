9d33b0196689f0586fb796d744b32713d72295ad
Pete Otaqui <pete.otaqui@gft.com>
2016-09-05 06:28:10 +0100

    Removed console.log

--- src/js/reducers/navigation.js
+++ src/js/reducers/navigation.js
@@ -10,7 +10,6 @@ export default function navigation(state = defaultNavigationState, action) {
   switch(action.type) {
     case SET_NAVIGATION:
       nav = JSON.parse(JSON.stringify(action.navigation || {}));
-      console.log('nav', nav);
       return Object.assign({}, state, nav);
     default:
       return state;
